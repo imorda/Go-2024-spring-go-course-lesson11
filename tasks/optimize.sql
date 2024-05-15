cb0b44faf1255118a50a2c1a7d890189ab0be31a
imorda <btimka1@gmail.com>
2024-05-15 05:17:20 +0300

    Added explain results

--- tasks/optimize.sql
+++ tasks/optimize.sql
@@ -1,22 +1,25 @@
 -- Выведите максимальную температуру по дням для датчика c sensor_id=12 отсортированного по убыванию температуры
 
--- Оценка сложности запроса до добавления индекса
-explain select date(timestamp) as Date, max(payload) as Temperature from go_course_db.events
-where sensor_id = 12
-group by Date
-order by Temperature desc;
-
 -- Добавление индекса
 create index if not exists sens_id_events on go_course_db.events(sensor_id);
 
--- Оценка сложности запроса после добавления индекса
-explain select date(timestamp) as Date, max(payload) as Temperature from go_course_db.events
-where sensor_id = 12
-group by Date
-order by Temperature desc;
 
 -- Температура по дням для датчика с sensor_id=12, отсортированная по убыванию температуры
 select date(timestamp) as Date, max(payload) as Temperature from go_course_db.events
 where sensor_id = 12
 group by Date
 order by Temperature desc;
+
+-- Оценка сложности запроса
+--   explain select date(timestamp) as Date, max(payload) as Temperature from go_course_db.events
+--   where sensor_id = 12
+--   group by Date
+--   order by Temperature desc;
+--
+-- Sort  (cost=255.56..261.54 rows=2392 width=12)
+--   Sort Key: (max(payload)) DESC
+--   ->  HashAggregate  (cost=91.42..121.32 rows=2392 width=12)
+-- "        Group Key: date(""timestamp"")"
+--         ->  Index Scan using sens_id_events on events  (cost=0.57..79.45 rows=2394 width=12)
+--               Index Cond: (sensor_id = 12)
+
